3dde9b0f2add66fd3bbd5d20136b572dd65f836a
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-07-27 22:05:35 -0300

    first commit

--- src/main/java/com/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/calculadora/controller/CalculadoraController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraController {
